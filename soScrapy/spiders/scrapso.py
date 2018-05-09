aa6e046b640ce987abedd754bcc627af36b5ee29
Boming Fan <bomingfan@bomings-mbp.lan>
2018-05-08 21:11:46 -0700

    added folder

---
+++ soScrapy/spiders/scrapso.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+import scrapy
+
+
+class ToScrapeSpider(scrapy.Spider):
+    name = "scrapso"
+    start_urls = [
+        'https://stackoverflow.com/jobs?sort=p&pg=122',
+    ]
+
+    def parse(self, response):
+        for job in response.css("div.-job-summary"):
+            yield {
+                'name': job.css("h2.g-col10 > a::attr(title)").extract_first(),
+                'tags': job.css("p > a.post-tag::text").extract()
+            }
+
+        next_page_url = response.css("div.pagination > a.test-pagination-next::attr(href)").extract_first()
+        if next_page_url is not None:
+            yield scrapy.Request(response.urljoin(next_page_url))
